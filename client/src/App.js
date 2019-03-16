3b7bf16a865992f93f8640836eca450b9d852db2
TextError <savinraul1983@yahoo.com>
2019-03-16 18:25:42 +0000

    more routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Landing from './components/landing/Landing';
-import Home from './components/home/Home';
 import Navbar from './layout/Navbar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
 import Footer from './layout/Footer';
 
 // Redux
@@ -22,6 +24,8 @@ class App extends Component {
               <div className='container-fluid'>
                 <Navbar />
                 <Route exact path='/home' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/projects' component={Projects} />
                 <Footer />
               </div>
             </Router>
