01bf3f309c15f1accc3e1cda1f320e72996619da
TextError <savinraul1983@yahoo.com>
2019-03-16 17:12:21 +0000

    few basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
+import Landing from './components/landing/Landing';
+import Home from './components/home/Home';
+import Navbar from './layout/Navbar';
+import Footer from './layout/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        app
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Router>
+              <div className='container-fluid'>
+                <Navbar />
+                <Route exact path='/home' component={Home} />
+                <Footer />
+              </div>
+            </Router>
+          </Switch>
+        </Router>
       </div>
     );
   }
