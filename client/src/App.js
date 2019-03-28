8d35b7740707fcba773606baf75dfbf82975aeb1
TextError <savinraul1983@yahoo.com>
2019-03-28 11:00:37 +0000

    Border, OpenBtn & CloseBtn component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Landing from './components/landing/Landing';
-import Navbar from './layout/Navbar';
+import Navbar from './layout/Navbar/Navbar';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
