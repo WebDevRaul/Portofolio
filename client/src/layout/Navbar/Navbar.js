d73ae65e35900c4c9f1f05878e359cae4a2a5348
TextError <savinraul1983@yahoo.com>
2019-03-29 12:06:16 +0000

    finish later

--- client/src/layout/Navbar/Navbar.js
+++ client/src/layout/Navbar/Navbar.js
@@ -19,10 +19,13 @@ class Navbar extends Component {
       second: false,
       third: false,
       closeBorder: false,
+      openBorderBack: false,
+      close: false,
     }
   }
 
   onButton = () => {
+    const { close, openBorder } = this.state;
     // const timer = setTimeout(() => {this.setState({ show: !this.state.show })}, 1000);
     this.setState({ openBorder: !this.state.openBorder });
 
@@ -30,19 +33,24 @@ class Navbar extends Component {
     const func = (data, seconds) => {
       setTimeout(() => { this.setState({ [data]: !this.state[data] }) }, seconds);
     };
+    // Border
     func('hideBorder', 500);
-    func('first', 800);
-    func('second', 1300);
-    func('third', 1500);
     func('hideBorder', 2500);
     func('closeBorder', 2500);
     func('openBorder', 3000);
     func('closeBorder', 3000);
-    
+
+    // Open
+    func('first', 800);
+    func('second', 1300);
+    func('third', 1500);
+    func('close', 1500);
+    if(close && openBorder) return func('openBorderBack', 2500);
+
    };
 
   render() {
-    const { show, openBorder, hideBorder, first, second, third, closeBorder } = this.state;
+    const { show, openBorder, hideBorder, first, second, third, closeBorder, openBorderBack } = this.state;
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
         <Logo />
@@ -51,8 +59,8 @@ class Navbar extends Component {
           onClick={this.onButton}
         >
         <MobileView 
-          border={{ openBorder, hideBorder, closeBorder }}
-          openBtn={{ first, second, third }}
+          border={{ openBorder, hideBorder, closeBorder, openBorderBack }}
+          openBtn={{ openBorder, first, second, third }}
           closeBtn={{  }}
           />
         </div>
--- client/src/layout/Navbar/OpenBtn.js
+++ client/src/layout/Navbar/OpenBtn.js
@@ -3,12 +3,12 @@ import classnames from 'classnames';
 
 export default class OpenBtn extends Component {
   render() {
-    const { hide, first, second, third } = this.props.openBtn;
+    const { openBorder, first, second, third, openBorderBack } = this.props.openBtn;
     return (
       <div className='open-div'>
-        <i className={classnames("", { 'animation': hide, 'hide' : first, })} ></i>
-        <i className={classnames("mt-2", { 'animation-two': hide, 'hide' : second })}></i>
-        <i className={classnames("mt-3", { 'animation-tree': hide, 'hide' : third })}></i>
+        <i className={classnames("", {'hide' : first, 'animationBack' : openBorderBack })} ></i>
+        <i className={classnames("mt-2", {'hide' : second, 'animationBack-two': openBorderBack })}></i>
+        <i className={classnames("mt-3", {'hide' : third, 'animationBack-tree': openBorderBack })}></i>
       </div>
     )
   }
