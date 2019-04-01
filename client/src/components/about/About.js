c8d9ff84322010f28309c6657928cb0a933b5790
TextError <savinraul1983@yahoo.com>
2019-04-01 21:03:35 +0100

    about me

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 // Components
 import Title from './Title';
+import AboutMe from './AboutMe';
 
 export default class About extends Component {
   render() {
@@ -11,6 +12,11 @@ export default class About extends Component {
           <div className='row'>
             <div className='col'>
               <Title />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <AboutMe />
             </div>
           </div>
         </div>
---
+++ client/src/components/about/AboutMe.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div className='about-me text-center'>
+      <div className='row'>
+        <div className='col'>
+          photo here
+        </div>
+        <div className='col'>
+          about me text
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <div>
+            more about me
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
