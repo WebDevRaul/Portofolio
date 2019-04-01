0eacfe92e366c472c3f579da4773052c7d3343e5
TextError <savinraul1983@yahoo.com>
2019-04-01 21:00:00 +0100

    title component

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// Components
+import Title from './Title';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        about
+      <div className='about'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col'>
+              <Title />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ client/src/components/about/Title.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className='title text-center'>
+      <div className='row'>
+        <div className='col'>
+          <div>
+            <h3>About <span className='heading'>Me</span></h3>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <div className='text-uppercase'>
+            <p>Allow me to introduce myslef</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
