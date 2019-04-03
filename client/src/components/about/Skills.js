632345ebba7d9335db1614d7c18f5483628e9d48
TextError <savinraul1983@yahoo.com>
2019-04-03 18:55:10 +0100

    PostgreSQL svg

--- client/src/components/about/Skills.js
+++ client/src/components/about/Skills.js
@@ -10,6 +10,7 @@ import '../../css/about.css';
 import Redux from '../../assets/Redux';
 import MongoDB from '../../assets/MongoDB';
 import NodeJs from '../../assets/NodeJs';
+import PostgreSQL from '../../assets/PostgreSQL';
 
 export default function Skills() {
   return (
@@ -25,6 +26,7 @@ export default function Skills() {
             <Circle svg={<Redux width='50px' />} text='Redux' />
             <Circle svg={<MongoDB height='3em' width='5.51em' />} text='MongoDB' />
             <Circle svg={<NodeJs height='3em' width='5.51em' />} text='NodeJs' />
+            <Circle svg={<PostgreSQL height='45px' width='45px' />} text='PostgreSQL' />
           </div>
         </div>
       </div>
