5c7215a4db43ebafc20c0716be9f12706257f4f7
TextError <savinraul1983@yahoo.com>
2019-04-05 17:56:57 +0100

    skill responsive

--- client/src/components/about/Skills.js
+++ client/src/components/about/Skills.js
@@ -20,19 +20,63 @@ import Javascript from '../../assets/Javascript';
 export default function Skills() {
   const rem = '3rem';
   return (
-    <div className='skills text-center'>
-      <div className='row'>
+    <div className='skills'>
+      <div className='row no-gutters'>
         <div className='col'>
-          <div className='d-flex p-2'>
-            <Circle svg={<Html width={rem} height={rem} />} text='Html 5' />
-            <Circle svg={<Css width={rem} height={rem} />} text='Css 3' />
-            <Circle svg={<Javascript width={rem} height={rem} />} text='Javascript' />
-            <Circle svg={<JQuery width={rem} height={rem} />} text='JQuery' />
-            <Circle svg={<ReactJs width={rem} height={rem} />} text='ReactJs' />
-            <Circle svg={<Redux width={rem} height={rem} />} text='Redux' />
-            <Circle svg={<MongoDB height={rem} width='5.5rem' />} text='MongoDB' />
-            <Circle svg={<NodeJs height={rem} width='5.5rem' />} text='NodeJs' />
-            <Circle svg={<PostgreSQL height={rem} width={rem} />} text='PostgreSQL' />
+          <div className='row no-gutters'>
+            <div className='col-md-6'>
+              <div className='row no-gutters'>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+                    <Circle svg={<Html width={rem} height={rem} />} text='Html 5' />
+                    <Circle svg={<Css width={rem} height={rem} />} text='Css 3' />
+                  </div>
+                </div>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+                    <Circle svg={<Javascript width={rem} height={rem} />} text='Javascript' />
+                    <Circle svg={<JQuery width={rem} height={rem} />} text='JQuery' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-6'>
+              <div className='row no-gutters'>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+                    <Circle svg={<ReactJs width={rem} height={rem} />} text='ReactJs' />
+                    <Circle svg={<Redux width={rem} height={rem} />} text='Redux' />
+                  </div>
+                </div>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+                    <Circle svg={<MongoDB height={rem} width='5.5rem' />} text='MongoDB' />
+                    <Circle svg={<NodeJs height={rem} width='5.5rem' />} text='NodeJs' />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='col-md-6'>
+              <div className='row no-gutters'>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+                    <Circle svg={<PostgreSQL height={rem} width={rem} />} text='PostgreSQL' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-6'>
+              <div className='row no-gutters'>
+                <div className='col-sm-6'>
+                  <div className='d-flex'>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
       </div>
