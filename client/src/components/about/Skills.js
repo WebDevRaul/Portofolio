8fce22892d81306b0701d7f2fcec9ca235f83a8a
TextError <savinraul1983@yahoo.com>
2019-04-05 16:30:05 +0100

    javascript

--- client/src/components/about/Skills.js
+++ client/src/components/about/Skills.js
@@ -18,20 +18,21 @@ import Css from '../../assets/Css';
 import Javascript from '../../assets/Javascript';
 
 export default function Skills() {
+  const rem = '3rem';
   return (
     <div className='skills text-center'>
       <div className='row'>
         <div className='col'>
           <div className='d-flex p-2'>
-            <Circle svg={<Html width='45px' height='45px' />} text='Html 5' />
-            <Circle svg={<Css width='45px' height='45px' />} text='Css 3' />
-            <Circle svg={<Javascript width='45px' height='45px' />} text='Javascript' />
-            <Circle svg={<JQuery width='50px' height='50px' />} text='JQuery' />
-            <Circle svg={<ReactJs width='50px' height='50px' />} text='ReactJs' />
-            <Circle svg={<Redux width='50px' />} text='Redux' />
-            <Circle svg={<MongoDB height='3em' width='5.51em' />} text='MongoDB' />
-            <Circle svg={<NodeJs height='3em' width='5.51em' />} text='NodeJs' />
-            <Circle svg={<PostgreSQL height='45px' width='45px' />} text='PostgreSQL' />
+            <Circle svg={<Html width={rem} height={rem} />} text='Html 5' />
+            <Circle svg={<Css width={rem} height={rem} />} text='Css 3' />
+            <Circle svg={<Javascript width={rem} height={rem} />} text='Javascript' />
+            <Circle svg={<JQuery width={rem} height={rem} />} text='JQuery' />
+            <Circle svg={<ReactJs width={rem} height={rem} />} text='ReactJs' />
+            <Circle svg={<Redux width={rem} height={rem} />} text='Redux' />
+            <Circle svg={<MongoDB height={rem} width='5.5rem' />} text='MongoDB' />
+            <Circle svg={<NodeJs height={rem} width='5.5rem' />} text='NodeJs' />
+            <Circle svg={<PostgreSQL height={rem} width={rem} />} text='PostgreSQL' />
           </div>
         </div>
       </div>
