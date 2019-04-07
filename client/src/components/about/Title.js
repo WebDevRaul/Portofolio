417d95d2dc747ed4d9585031aad4df4291ba1943
TextError <savinraul1983@yahoo.com>
2019-04-07 15:57:13 +0100

    about me border animation

--- client/src/components/about/Title.js
+++ client/src/components/about/Title.js
@@ -5,19 +5,19 @@ export default function Title() {
     <div className='title text-center'>
       <div className='row mb-5 mt-5 no-gutters'>
         <div className='col'>
-          <div className='title-div'>
-            <h3 className='m-0'>
-              <i></i>
-              <span className='title-span'>About Me</span>
-              <i></i>
-            </h3>
+          <div className='title-div m-auto div-animation'>
+            <i className='border-animation'></i>
+              <h3 className='d-flex'>
+                <span className='title-text p-2 title-animation'>About Me</span>
+              </h3>
+            <i className='border-animation'></i>
           </div>
         </div>
       </div>
       <div className='row'>
         <div className='col'>
-          <div className='text-uppercase'>
-            <p>Allow me to introduce myslef</p>
+          <div className='subtitle text-uppercase mb-3'>
+            <span className=''>Allow me to introduce myself</span>
           </div>
         </div>
       </div>
