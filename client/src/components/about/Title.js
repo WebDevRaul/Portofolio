8e4a2ffa2e54c08c210fef38a05edbb431c84e36
TextError <savinraul1983@yahoo.com>
2019-04-07 16:32:10 +0100

    prepare animation subtitle

--- client/src/components/about/Title.js
+++ client/src/components/about/Title.js
@@ -16,8 +16,38 @@ export default function Title() {
       </div>
       <div className='row'>
         <div className='col'>
-          <div className='subtitle text-uppercase mb-3'>
-            <span className=''>Allow me to introduce myself</span>
+          <div className='subtitle-div text-uppercase mb-3'>
+            {/* <span className=''>Allow me to introduce myself</span> */}
+            <div className='subtitle-text'>
+              <span>A</span>
+              <span>l</span>
+              <span>l</span>
+              <span>o</span>
+              <span>w</span>
+              <span className='space'></span>
+              <span>m</span>
+              <span>e</span>
+              <span className='space'></span>
+              <span>t</span>
+              <span>o</span>
+              <span className='space'></span>
+              <span>i</span>
+              <span>n</span>
+              <span>t</span>
+              <span>r</span>
+              <span>o</span>
+              <span>d</span>
+              <span>u</span>
+              <span>c</span>
+              <span>e</span>
+              <span className='space'></span>
+              <span>m</span>
+              <span>y</span>
+              <span>s</span>
+              <span>e</span>
+              <span>l</span>
+              <span>f</span>
+            </div>
           </div>
         </div>
       </div>
