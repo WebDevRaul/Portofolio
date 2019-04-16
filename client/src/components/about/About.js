bd4e302184bcac7d08393e54e60901a65c5c500c
TextError <savinraul1983@yahoo.com>
2019-04-16 17:21:24 +0100

    wow.js

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import WOW from "wow.js";
 
 // Components
 import Title from './Title';
@@ -13,57 +14,18 @@ import '../../css/about.css';
 export default class About extends Component {
   state = {
     active: false,
-    animateSkill: false,
-    animateAboutMe: false,
-    position: 0,
-    isScrolling: false,
   }
 
   componentDidMount() {
     this.setState({ active: true });
-    window.addEventListener("scroll", this.throttleScroll, false);
+    const wow = new WOW()
+    wow.init();
   };
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.throttleScroll)
-  }
-
-  throttleScroll = (e) => {
-    const { isScrolling } = this.state;
-    if (isScrolling === false) {
-      window.requestAnimationFrame(() => {
-        this.scrolling(e);
-        this.setState({ isScrolling: false })
-      });
-    }
-    this.setState({ isScrolling: true })
-  }
-
-  scrolling = (e) => {
-    if(this.aboutMeBioFirst) this.setState({ animateAboutMe: true })
-  }
-
-  // Callback from AboutMe component
-  aboutMeBioFirst = element => {
-    const top = element.top;
-    const bottom = element.bottom;
- 
-    return ((top >= 0) && (bottom <= window.innerHeight))
-  }
-  aboutMeBioSecond = element => {
-    console.log(element)
-  }
-  aboutMeBioThird = element => {
-    console.log(element)
-  }
-
-
-
   render() {
-    console.log(this.state.animateAboutMe);
-    const { active, animateSkill, animateAboutMe } = this.state;
+    const { active } = this.state;
     return (
-      <div className='about' onScroll={this.listenToScroll}>
+      <div className='about'>
         <div className='container-fluid p-0'>
           <div className='row no-gutters'>
             <div className='col'>
@@ -72,17 +34,12 @@ export default class About extends Component {
           </div>
           <div className='row no-gutters'>
             <div className='col'>
-              <AboutMe 
-                animation={animateAboutMe} 
-                first={this.aboutMeBioFirst}
-                second={this.aboutMeBioSecond}
-                third={this.aboutMeBioThird}
-              />
+              <AboutMe />
             </div>
           </div>
           <div className='row no-gutters'>
             <div className='col'>
-              <Skills animate={animateSkill} />
+              <Skills />
             </div>
           </div>
           <div className='row no-gutters'>
--- client/src/components/about/AboutMe.js
+++ client/src/components/about/AboutMe.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
-import classnames from 'classnames';
 
 // Photo
 import Photo from '../../assets/Photo.jpg'
 
 class AboutMe extends Component {
-  first = element => {
-    if(element) return this.props.first(element.getBoundingClientRect(element));
-  }
-  second = element => {
-    if(element) return this.props.second(element.getBoundingClientRect(element));
-  }
-  third = element => {
-    if(element) return this.props.third(element.getBoundingClientRect(element));
-  }
   render() {
-    const { animation } = this.props;
     return (
       <div className='about-me text-center'>
         <div className='row no-gutters'>
@@ -26,21 +15,13 @@ class AboutMe extends Component {
           </div>
           <div className='col col-md-7 mt-5'>
             <div className='about-me-bio p-3'>
-              <p 
-                ref={this.first} 
-                className={classnames('', {'animation-left': animation})}
-              >
-                <span className={classnames('about-me-first-letter', {'animation-left': animation})}>H</span>ello! My name is Savin Raul-Calin, I`m 35y old currently living in UK.<br />
+              <p className={this.props.animation}>
+                <span className='about-me-first-letter'>H</span>ello! My name is Savin Raul-Calin, I`m 35y old currently living in UK.<br />
                 At the end of 2017 I wrote the first <i>Hello World</i>, and since then I have been learning to code, using diffrent sources of information and dedicating every minute of my spare time for this passion.
               </p>
               <p 
-                ref={this.second}
-                className={classnames('', {'animation-right': animation})} 
               >As a person I`m communicative and friendly. Over the years, the jobs i performed always involved being in contact with people and being part of the team.</p>
-              <p
-                ref={this.third} 
-                className={classnames('', {'animation-left': animation})} 
-              >I`m a dedicated self learner and problem solver. Programming has challenged me a lot, but hard work, curiosity and persistence helped me evolve.</p>
+              <p>I`m a dedicated self learner and problem solver. Programming has challenged me a lot, but hard work, curiosity and persistence helped me evolve.</p>
             </div>
           </div>
         </div>
