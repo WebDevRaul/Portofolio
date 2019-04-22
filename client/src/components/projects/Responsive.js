a6026137d9795710d99070beea05badba346456b
TextError <savinraul1983@yahoo.com>
2019-04-22 19:49:39 +0100

    litle of responsive map

--- client/src/components/projects/Responsive.js
+++ client/src/components/projects/Responsive.js
@@ -4,8 +4,23 @@ import React, { Component } from 'react';
 export default class Responsive extends Component {
   render() {
     return (
-      <div>
-        <p>Img of pc,laptop & smartphone</p>
+      <div className='responsive'>
+        <div className='container'>
+          <div className='row no-gutters'>
+            <div className='col'>
+              <h5>Responsive Websites ...</h5>
+            </div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='col'>
+              <div className='responsive-img-div d-flex'>
+                <div className='m-auto'>
+                  img of pc,laptop & smartphone
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
