63895d17c21bdc75e2714942c8cf6989f6968fa5
TextError <savinraul1983@yahoo.com>
2019-04-23 19:08:06 +0100

    prepare for animation

--- client/src/components/projects/Title.js
+++ client/src/components/projects/Title.js
@@ -7,13 +7,27 @@ export default class Title extends Component {
   render() {
     return (
       <div className='project-title'>
-        <div className='container'>
-          <div className='d-flex bg-white'>
-            <div className='m-auto project-title-div'>
-              <h5 className='mb-0 text-center'>My Recent<span className='project-title-div-heading ml-2 mb-0'>Projects</span></h5>
-              <p className='mt-4 m-0'>Here are a few recent design projects.</p>
+        <div className='container bg-white'>
+            <div className='row no-gutters'>
+              <div className='col'>
+                <div className='d-flex'>
+                  <div className='m-auto project-title-main-title'>
+                      <div className='d-flex'>
+                        <h5 className='text-center m-auto'>My Recent</h5><span className='project-title-div-heading ml-2 mb-0'>Projects</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+            <div className='row no-gutters'>
+              <div className='col'>
+                <div className='d-flex'>
+                  <div className='project-title-subtitle m-auto'>
+                    <p className='mt-4 m-0'>Here are a few recent design projects.</p>  
+                  </div>
+                </div>  
+              </div>
+            </div>
         </div>
       </div>
     )
