a4f7def19137bd6bb6dee6fe547a1396861fa93c
TextError <savinraul1983@yahoo.com>
2019-05-11 18:33:42 +0100

    0 to 100

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 
+// Css
+import '../../css/landing.css';
+
 class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0
+    }
+  };
 
   componentDidMount() {
-    setInterval(() => this.props.history.push('/home'), 5000);
-  }
+    this.interval = setInterval(() => this.setState({ number: this.state.number + 1 }), 30);
+  };
+
+  componentDidUpdate() {
+    const { number } = this.state;
+    // Clear interval when reach 100
+    if (number === 100) {
+      clearInterval(this.interval);
+    };
+    // Redirect on home when cicle i complete
+    // if (number === 100) {
+    //   setInterval(() => this.props.history.push('/home'), 2000);
+    // }
+  };
+
   render() {
     return (
-      <div>
-        Landing
+      <div className='landing'>
+        <div className='container'>
+          <div className='row no-gutters'>
+            <div className='col'>
+              <div className='landing-loading d-flex'>
+                <h1 className='m-auto'>{this.state.number} %</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
