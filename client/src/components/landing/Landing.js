c29cbc3f367468fc04157aec8080d6dd825b2225
TextError <savinraul1983@yahoo.com>
2019-05-11 19:44:37 +0100

    fadeUp

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import { withRouter } from "react-router-dom";
+
+// Animation
+import WOW from "wow.js";
 
 // Css
 import '../../css/landing.css';
@@ -8,34 +12,45 @@ class Landing extends Component {
   constructor() {
     super();
     this.state = {
-      number: 0
+      number: 0,
+      cicle: false
     }
   };
 
   componentDidMount() {
+    // Init the WOW
+    const wow = new WOW();
+    wow.init();
+    // Start interval
     this.interval = setInterval(() => this.setState({ number: this.state.number + 1 }), 30);
   };
 
   componentDidUpdate() {
-    const { number } = this.state;
+    const { number, cicle } = this.state;
     // Clear interval when reach 100
     if (number === 100) {
       clearInterval(this.interval);
     };
+    // Start animation
+    if ((number === 100) && (cicle === false)) {
+      this.setState({ cicle: true })
+    }
     // Redirect on home when cicle i complete
     // if (number === 100) {
-    //   setInterval(() => this.props.history.push('/home'), 2000);
-    // }
-  };
-
-  render() {
+    //     setInterval(() => this.props.history.push('/home'), 2000);
+    //   }
+    };
+    
+    render() {
+      const { number, cicle } = this.state;
+      console.log(cicle)
     return (
       <div className='landing'>
         <div className='container'>
           <div className='row no-gutters'>
             <div className='col'>
               <div className='landing-loading d-flex'>
-                <h1 className='m-auto'>{this.state.number} %</h1>
+                <h1 className={classnames('m-auto', {'fadeUp':cicle})}>{number} %</h1>
               </div>
             </div>
           </div>
