1157bd1896637b0e17a81564198ce89b6c6079ef
TextError <savinraul1983@yahoo.com>
2019-05-25 17:51:43 +0100

    map responsive

--- client/src/layout/Footer/Footer.js
+++ client/src/layout/Footer/Footer.js
@@ -3,17 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 // Components
 import Form from './Form';
-import Map from './MapViewer';
+import MapViewer from './MapViewer';
+
+// Css
+import '../../css/footer.css';
 
 export default class Footer extends Component {
   render() {
     return (
       <div className='footer bg-dark text-white'>
-        <div className='row no-gutters pt-3'>
-          <div className='col-sm-12 col-md-6'>
-            <Map />
+        <div className='row no-gutters pt-3 pl-2 pr-2'>
+          <div className='col-12 col-sm-6 m-auto'>
+            <MapViewer />
           </div>
-          <div className='col-sm-12 col-md-6'>
+          <div className='col-12 col-sm-6'>
             <Form />
           </div>
         </div>
--- client/src/layout/Footer/MapViewer.js
+++ client/src/layout/Footer/MapViewer.js
@@ -7,19 +7,19 @@ import { API_GOOGLE_MAP } from '../../config/googleMap';
 class MapViewer extends Component {
   render() {
     const mapStyles = {
+      height: '300px', 
       width: '100%',
-      height: '100%',
     };
     return (
-      <div className='pl-2 pr-2'>
-        <div className=''>
+      <div className='map-viewer'>
+        <div className='map'>
           <Map
             google={this.props.google}
             zoom={8}
             style={mapStyles}
-            initialCenter={{ lat: 51.4543, lng: 0.9781}}
+            initialCenter={{ lat: 51.4543, lng: -0.9781}}
           >
-            <Marker position={{ lat: 51.4543, lng: 0.9781}} />
+            <Marker position={{ lat: 51.4543, lng: -0.9781}} />
           </Map>
         </div>
       </div>
