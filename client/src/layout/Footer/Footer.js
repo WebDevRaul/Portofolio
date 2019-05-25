933a23b0569ebbc32460fecabd6ee5dbe1f82ea1
TextError <savinraul1983@yahoo.com>
2019-05-25 18:00:21 +0100

    links

--- client/src/layout/Footer/Footer.js
+++ client/src/layout/Footer/Footer.js
@@ -20,40 +20,43 @@ export default class Footer extends Component {
             <Form />
           </div>
         </div>
+        <div className='row no-gutters mt-3 mb-3'>
+          <div className='col-12 col-sm-6'>
+            
+          </div>
+          <div className='col-12 col-sm-6'>
+            <div className='social-media'>
+              <span className='p-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='fab fa-facebook-f fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='p-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='fab fa-twitter fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='p-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='fab fa-instagram fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='p-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='fab fa-linkedin-in fa-1x'></i>
+                </NavLink>
+              </span>
+            </div>
+          </div>
+        </div>
         <div className='row no-gutters'>
           <div className='col m-auto'>
-            <div className='text-center'>
+            <div className='text-center m-3'>
               <span>Copyright &copy; {new Date().getFullYear()} <span className='ml-2 mr-2'>by</span> Savin Raul-Calin</span>
             </div>
           </div>
         </div>
-        {/* <div className='row no-gutters'>
-          <div className='col'>
-            <div>
-              <span className='ml-2 mr-2'>
-                <NavLink to={'/'} target='_blank'>
-                  <i className='m-2 fab fa-facebook-f fa-1x'></i>
-                </NavLink>
-              </span>
-              <span className='ml-2 mr-2'>
-                <NavLink to={'/'} target='_blank'>
-                  <i className='m-2 fab fa-twitter fa-1x'></i>
-                </NavLink>
-              </span>
-              <span className='ml-2 mr-2'>
-                <NavLink to={'/'} target='_blank'>
-                  <i className='m-2 fab fa-instagram fa-1x'></i>
-                </NavLink>
-              </span>
-              <span className='ml-2 mr-2'>
-                <NavLink to={'/'} target='_blank'>
-                  <i className='m-2 fab fa-linkedin-in fa-1x'></i>
-                </NavLink>
-              </span>
-            </div>
-          </div>
-        </div> */}
       </div>
-    )
-  }
-}
+    );
+  };
+};
