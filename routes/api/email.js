d1e61f69c385e4652f8fcfac62c4e2c5cc97bacb
TextError <savinraul1983@yahoo.com>
2019-08-04 18:34:56 +0100

    litle fix

--- routes/api/email.js
+++ routes/api/email.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router  = express.Router();
 const nodeMailer = require('nodemailer');
-const user = require('../../config/keys');
+const { email, password } = require('../../config/keys');
 
 // Validation
 const validateSendEmail = require('../../validation/email');
@@ -16,19 +16,18 @@ router.post('/send-email', (req, res) => {
     // If any errors, send 400 with errors object
     return res.status(400).json(errors);
   } else {
-    const me = user.email
     let transporter = nodeMailer.createTransport({
       host: 'smtp.gmail.com',
       port: 465,
       secure: true,
       auth: {
-        user: me,
-        pass: user.password
+        user: email,
+        pass: password
       }
     });
     let mailOptions = {
       from: req.body.email, // sender address
-      to: me, // list of receivers
+      to: email, // list of receivers
       subject: `E-mail send by ${req.body.email}`, // Subject line
       text: req.body.textarea // plain text body
     };
