91d0ee736b5ad305806da67d0a19bc215aaefc9f
TextError <savinraul1983@yahoo.com>
2019-08-04 18:38:46 +0100

    redirect to /

--- client/src/components/common/NotFound.js
+++ client/src/components/common/NotFound.js
@@ -13,7 +13,7 @@ class NotFound extends Component {
             <h1 className='m-0'>Oops!</h1> 
             <h4 className='text-center'>404 - Page not found</h4>
             <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
-            <NavLink to='/home' className='nav-link text-center' onClick={this.onClick}>
+            <NavLink to='/' className='nav-link text-center' onClick={this.onClick}>
               <span className='btn btn-primary'>Go to homepage</span>
             </NavLink>
           </div>
