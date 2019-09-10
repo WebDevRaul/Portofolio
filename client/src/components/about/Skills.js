5d6088b48cfb262eacfc4da9b4706c22cd299162
TextError <savinraul1983@yahoo.com>
2019-09-10 16:29:52 +0100

    fix responsive skills

--- client/src/components/about/Skills.js
+++ client/src/components/about/Skills.js
@@ -23,7 +23,7 @@ class Skills extends Component {
             <p className={classnames('', {'zoomIn': skills, 'opacity': !skills})}>Up to the present moment I have gained experience in the following:</p> 
             <div className={classnames('', {'fadeInUp': skills, 'opacity': !skills})}>
               <div className='row no-gutters'>
-                <div className='col-lg-5 offset-lg-2'>
+                <div className='col-lg-8 offset-lg-2'>
                   <div className='row no-gutters'>
                     <div className='col-md-12'>
                       <div className='row no-gutters'>
@@ -44,20 +44,23 @@ class Skills extends Component {
                       </div>
                     </div>
                   </div>
-                </div>
-                  <div className='col'>
-                    <div className='row no-gutters'>
-                      <div className='col-sm-6'>
-                        <div className='d-flex'>
-                          <Circle svg={<Logo.NodeJs height={rem} width='5.5em' />} text='NodeJs' />
-                          <Circle svg={<Logo.MongoDB height={rem} width='5.5em' />} text='MongoDB' />
-                          <Circle svg={<Logo.PostgreSQL height={rem} width={rem} />} text='PostgreSQL' />
+                  <div className='row no-gutters'>
+                    <div className='col-md-12'>
+                      <div className='row no-gutters'>
+                        <div className='col-sm-6'>
+                          <div className='d-flex'>
+                            <Circle svg={<Logo.NodeJs height={rem} width='5.5em' />} text='NodeJs' />
+                            <Circle svg={<Logo.MongoDB height={rem} width='5.5em' />} text='MongoDB' />
+                            <Circle svg={<Logo.PostgreSQL height={rem} width={rem} />} text='PostgreSQL' />
+                          </div>
                         </div>
-                      </div>
-                      <div className='col-sm-6'>
-                        <div className='last-update'>
-                          <Circle svg={<Logo.Sass height={rem} width={rem} />} text='Sass' />
-                          <Circle svg={<Logo.GraphQl height={rem} width={rem} />} text='GraphQl' />
+                        <div className='col-sm-6'>
+                          <div className='d-flex'>
+                            <Circle svg={<Logo.Sass height={rem} width={rem} />} text='Sass' />
+                            <Circle svg={<Logo.GraphQl height={rem} width={rem} />} text='GraphQl' />
+                            <div className='m-auto invisible'><Circle svg={<Logo.Html width={rem} height={rem} />} text='Html 5' /></div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
